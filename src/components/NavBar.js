ac0642edf5598760581b5714755c050871d80518
khalid <khalid.mohamed@moringaschool.com>
2023-11-05 20:07:50 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyle = {
+  textDecoration: "none",
+  marginRight: '15px',
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+     <NavLink to="/" style={linkStyle}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={linkStyle}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={linkStyle}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={linkStyle}>
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
